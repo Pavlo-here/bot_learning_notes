edc16fad373c48e705832bbede50e0f0c9a5ae90
P-is-here <pasha.nasonovhtc@gmail.com>
2023-07-11 13:15:54 +0300

    course2 start

---
+++ course2/asyncio_basic.py
@@ -0,0 +1,22 @@
+import asyncio
+
+
+async def send_hello() -> None:
+    await asyncio.sleep(2)
+    print('Hello')
+
+
+async def send_bye() -> None:
+    await asyncio.sleep(1)
+    print('Bye')
+
+
+async def main():
+    task_1 = asyncio.create_task(send_hello())
+    task_2 = asyncio.create_task(send_bye())
+
+    await task_1
+    await task_2
+
+
+asyncio.run(main())
